d663fbb5a88617a9d1bd3b82c0cffc8af48b4578
Akshay Rakhunde <115110322+akshayrakhunde01@users.noreply.github.com>
2024-01-03 22:44:14 +0530

    Your commit message here

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+
+
+// some garbadge
+
+import './App.css'
+import Product from "./Product"
+
+
+function App() {
+  return (
+    <>
+    <h1>Hello React (Main Component)</h1>
+      <Product name="One+7 ." />
+      <Product name="Samsung M51 ."/>
+      <Product name ="Nokia 8.1 ." />
+    
+    </>
+  );
+}
+
+
+export default App
---
+++ src/Product.jsx
@@ -0,0 +1,14 @@
+
+function Product({name}){
+    return(
+        <>
+         <div className="Product">
+        <h3>{name}</h3>
+        
+        </div>
+        
+        </>
+    )
+}
+
+export default Product
